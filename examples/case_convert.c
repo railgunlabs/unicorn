68eedee471084178066173b76674068543aa9397
hgs3 <82091+hgs3@users.noreply.github.com>
2025-11-15 19:28:27 -0600

    [skip ci] Disabling the examples by default.

--- examples/case_convert.c
+++ examples/case_convert.c
@@ -23,39 +23,6 @@ int main(int argc, char *argv[])
     // The following function call performs the case conversion.
     // It's documented online at:
     // <https://railgunlabs.com/unicorn/manual/api/case-mapping/uni-caseconv/>.
-    //
-    // In this example, here's what each argument means:
-    //
-    // UNI_UPPER :
-    //   Specifies we want upper case conversion (as opposed to
-    //   lower or title case conversion).
-    //
-    // src :
-    //   The text to be upper cased (i.e. the input text). 
-    //
-    // -1 :
-    //   A negative value indicates the input text is null terminated
-    //   which is true for this example. You can explictly pass the
-    //   number of code units if preferred (this is required when the
-    //   input is not null terminated).
-    //
-    // UNI_UTF8 :
-    //   The input is UTF-8 encoded.
-    //
-    // dest :
-    //   This is output or destination buffer (i.e. the buffer the
-    //   upper cased text will be written to).
-    //
-    // dest_len :
-    //   This must be set to the length of the destination buffer when
-    //   the function is called. The implementation writes to it the
-    //   number of code units written to the destination buffer.
-    //   It's neccessary to know how many code units were were
-    //   written when the output is not null terminated.
-    //
-    // UNI_UTF8 | UNI_NULIFY :
-    //   Bit flags that specify the resulting case converted text should
-    //   be written as UTF-8 and with a null terminator.
     if (uni_caseconv(UNI_UPPER, src, -1, UNI_UTF8, dest, &dest_len, UNI_UTF8 | UNI_NULIFY) == UNI_OK)
     {
         printf("Case converted: %s\n", dest);
